7ed2754d23e6a4258408f9f64496866a8b288983
Ajaykarki13 <115032238+Ajaykarki13@users.noreply.github.com>
2023-02-28 00:34:22 +0530

    corrections

--- index.js
+++ index.js
@@ -10,15 +10,39 @@ form.addEventListener("submit", validateForm);
 
 function validateForm(e) {
   e.preventDefault();
-  let data;
+  let data = {};
   if (password.value != confirm.value) {
     alert("password not matched");
   } else if (password.value == confirm.value) {
     data = { Email: email.value, Password: password.value, Name: name.value };
-    users.push(data);
-    //console.log(users)
-    localStorage.setItem("userinfo", JSON.stringify(users));
-    window.location.href = "./login.html";
+    console.log(data);
   }
+
+  const existingusers = localStorage.getItem("userinfo"); // checking if there is existing user
+  console.log(existingusers);
+
+  if (existingusers) {
+    //if yes parse it and store in users array
+    users = JSON.parse(localStorage.getItem("userinfo"));
+    console.log(users);
+
+    let checkuser = users.filter((x) => {
+      return x.Email === email.value;
+    });
+    if (checkuser.length == 0) {
+      //if (users.filter((x) => x.Email == email.value).length==0) {
+      users.push(data);
+      console.log(users);
+    } else {
+      document.getElementById("error-message").innerText =
+        "user with this email already exists";
+    }
+  } else {
+    //if no
+    users = [data];
+    console.log(users);
+  }
+
+  localStorage.setItem("userinfo", JSON.stringify(users));
+  window.location.href = "./login.html";
 }
-console.log(users);
